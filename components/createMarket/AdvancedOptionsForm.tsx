adb214683ebe9d91f69b8dc501aed102820dbb78
GonkSolana <154090283+GonkSolana@users.noreply.github.com>
2024-03-15 03:38:00 +0400

    Update AdvancedOptionsForm.tsx

--- components/createMarket/AdvancedOptionsForm.tsx
+++ components/createMarket/AdvancedOptionsForm.tsx
@@ -1,13 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Switch } from "@headlessui/react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormState, UseFormRegister, UseFormSetValue } from "react-hook-form";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { CreateMarketFormValues } from "../../pages/market/create";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { classNames } from "../../utils/general";
 
 type AdvancedOptionsFormProps = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   useAdvancedOptions: boolean;
   register: UseFormRegister<CreateMarketFormValues>;
   formState: FormState<CreateMarketFormValues>;
   setValue: UseFormSetValue<CreateMarketFormValues>;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   totalMarketAccountSizes: {
     totalEventQueueSize: number;
     totalRequestQueueSize: number;
@@ -15,10 +21,15 @@ type AdvancedOptionsFormProps = {
   };
 };
 export default function AdvancedOptionsForm({
+                                              // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                               useAdvancedOptions,
+                                              // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                               register,
+                                              // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                               setValue,
+                                                          // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                               formState: { errors },
+                                              // eslint-disable-next-line @typescript-eslint/no-unused-vars
                                               totalMarketAccountSizes,
                                             }: AdvancedOptionsFormProps) {
     return (
